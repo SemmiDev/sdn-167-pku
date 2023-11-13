03dbd27aed43013182c9f205fbc748a939a68b14
Sammi Aldhi Yanto <sammidev4@gmail.com>
2023-11-13 19:07:10 +0700

    feat: rps api

--- app/Http/Controllers/App/AbsensiController.php
+++ app/Http/Controllers/App/AbsensiController.php
@@ -9,7 +9,6 @@ use App\Models\Dokumentasi;
 use App\Models\Guru;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
 
 class AbsensiController extends Controller
 {
